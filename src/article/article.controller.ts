e0896e0973811a0b421e654610d019c819499f0d
Pasha <g1ebikpasha@gmail.com>
2023-04-26 19:49:48 +0800

    create article / get article by slug
    Signed-off-by: Pasha <g1ebikpasha@gmail.com>

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query, UseGuards } from "@nestjs/common";
 import { ArticleService } from "./article.service";
+import { AuthGuard } from "@app/user/guards/auth.guard";
+import { UserEntity } from "@app/user/user.entity";
+import { User } from "@app/user/decorators/user.decorator";
+import { CreateArticleDto } from "./dto/createArticle.dto";
+import { ArticleResponseInterface } from "./types/articleResponse.interface";
 
 @Controller('articles')
 export class ArticleController {
     constructor(private readonly articleService: ArticleService) { }
 
     @Post()
-    async create() {
-        return await this.articleService.createArticle()
+    @UseGuards(AuthGuard)
+    async create(
+        @User() currentUser: UserEntity,
+        @Body('article') createArticleDto: CreateArticleDto
+    ): Promise<ArticleResponseInterface> {
+        const article = await this.articleService.createArticle(currentUser, createArticleDto)
+        return this.articleService.buildArticleresponse(article)
+    }
+
+    //http://localhost:3000/articles?slug=some-test-title-4-a227kz
+    // @Get() //v2
+    // async getArticle(
+    //     @Query('slug') slug: string
+    // ): Promise<ArticleResponseInterface> {
+
+    //http://localhost:3000/articles/some-test-title-4-a227kz
+    @Get(':slug')
+    async getArticle(
+        @Param('slug') slug: string
+    ): Promise<ArticleResponseInterface> {
+        const article = await this.articleService.getArticleBySlug(slug)
+        return this.articleService.buildArticleresponse(article)
     }
 }
