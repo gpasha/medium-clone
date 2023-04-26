41024da7ed70f756e3f8dcc551fbcc387725e403
Pasha <g1ebikpasha@gmail.com>
2023-04-26 18:22:26 +0800

    Article Entity
    Signed-off-by: Pasha <g1ebikpasha@gmail.com>

---
+++ src/article/article.entity.ts
@@ -0,0 +1,34 @@
+import { BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'articles' })
+export class ArticleEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    slug: string
+
+    @Column({ default: '' })
+    description: string
+
+    @Column({ default: '' })
+    body: string
+
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    updatedAt: Date
+
+    @BeforeUpdate()
+    updateTimeStamp() {
+        this.updatedAt = new Date()
+    }
+
+    @Column('simple-array')
+    tagList: string[]
+
+    @Column({ default: 0 })
+    favoritesCount: number
+}
