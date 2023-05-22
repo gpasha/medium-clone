7c32e8a6cfa2f8d1a905c136b2157cfca6213529
Pasha <g1ebikpasha@gmail.com>
2023-05-22 23:39:45 +0800

    Follow
    Signed-off-by: Pasha <g1ebikpasha@gmail.com>

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,8 @@
 import { User } from "@app/user/decorators/user.decorator";
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { ProfileResponse } from "./types/profileResponse.interface";
 import { ProfileService } from "./profile.service";
+import { AuthGuard } from "@app/user/guards/auth.guard";
 
 @Controller('profiles')
 export class ProfileController {
@@ -13,6 +14,16 @@ export class ProfileController {
         @Param('username') profileUsername: string
     ): Promise<ProfileResponse> {
         const profile = await this.profileService.getProfile(currentUserId, profileUsername)
+        return this.profileService.buildProfileResponse(profile)
+    }
+
+    @Post(':username/follow')
+    @UseGuards(AuthGuard)
+    async followProfile(
+        @User('id') currentUserId: number,
+        @Param('username') profileUsername: string
+    ): Promise<ProfileResponse> {
+        const profile = await this.profileService.followProfile(currentUserId, profileUsername)
         return this.profileService.buildProfileResponse(profile)
     }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,10 +4,15 @@ import { ProfileType } from "./types/profile.type";
 import { ProfileResponse } from "./types/profileResponse.interface";
 import { UserEntity } from "@app/user/user.entity";
 import { Repository } from "typeorm";
+import { FollowEntity } from "./follow.entity";
 
 @Injectable()
 export class ProfileService {
-    constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) { }
+    constructor(
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+        @InjectRepository(FollowEntity) private readonly followRepository: Repository<FollowEntity>
+
+    ) { }
 
     async getProfile(currentUserId: number, profileUsername: string): Promise<ProfileType> {
         const user = await this.userRepository.findOne({
@@ -19,6 +24,34 @@ export class ProfileService {
         }
 
         return { ...user, following: false }
+    }
+
+    async followProfile(currentUserId: number, profileUsername: string): Promise<ProfileType> {
+        const user = await this.userRepository.findOne({
+            username: profileUsername
+        })
+
+        if (!user) {
+            throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND)
+        }
+
+        if (currentUserId === user.id) {
+            throw new HttpException('Follower and following can be equal', HttpStatus.BAD_REQUEST)
+        }
+
+        const follow = await this.followRepository.findOne({
+            followerId: currentUserId,
+            followingId: user.id
+        })
+
+        if (!follow) {
+            const followToCreate = new FollowEntity()
+            followToCreate.followerId = currentUserId
+            followToCreate.followingId = user.id
+            await this.followRepository.save(followToCreate)
+        }
+
+        return { ...user, following: true }
     }
 
     buildProfileResponse(profile: ProfileType): ProfileResponse {
